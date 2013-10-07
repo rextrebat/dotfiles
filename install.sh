e0b4a13a1240898a928028cc29567155f485d78a
Kingshuk Dasgupta <rextrebat@gmail.com>
2013-10-07 15:36:48 -0500

    zshrc and oh-my-zsh git link added to install.sh

--- install.sh
+++ install.sh
@@ -28,3 +28,5 @@ git submodule update
 git submodule foreach git pull origin master
 git submodule foreach git submodule init
 git submodule foreach git submodule update
+
+git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
