2fd65b7adad2df8430d300a0af53e82728d67295
Kingshuk Dasgupta <rextrebat@gmail.com>
2015-12-30 16:22:40 -0500

    Added Conky

---
+++ _conky/conky-startup.sh
@@ -0,0 +1,6 @@
+sleep 20s
+killall conky
+cd "/home/kdasgupta/.conky/TeejeeTech"
+conky -c "/home/kdasgupta/.conky/TeejeeTech/CPU Panel (4-core)" &
+cd "/home/kdasgupta/.conky/TeejeeTech"
+conky -c "/home/kdasgupta/.conky/TeejeeTech/Process Panel" &
